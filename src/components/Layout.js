c6167a11db287e4926e8a5eb8e4fd4d58b2437d8
RMcKnight <mcknight@Ryans-MacBook-Pro-5.local>
2019-07-05 11:48:18 -0400

    updates GA script code

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -41,7 +41,6 @@ const TemplateWrapper = ({ children }) => {
             <meta property="og:title" content={title} />
             <meta property="og:url" content="/" />
             <meta property="og:image" content="/img/og-image.jpg" />
-            <!-- Global site tag (gtag.js) - Google Analytics -->
             <script async src="https://www.googletagmanager.com/gtag/js?id=UA-143367766-1"></script>
             <script>
               window.dataLayer = window.dataLayer || [];
